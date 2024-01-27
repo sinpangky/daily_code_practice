4df700bcc4ef8d906cc8e453dd064c46fc49badf
sinpangky <sinpangky@gmail.com>
2024-01-27 13:19:14 +0000

    use setsockopt to cancel the wait time of a used port

---
+++ linux_learning/day27/setsockopt/tcp_chat_client.c
@@ -0,0 +1,56 @@
+#include <func.h>
+
+int main(int argc, char const *argv[]) {
+  ARGS_CHECK(argc, 3);
+  int sock;
+
+  struct sockaddr_in serv_address;
+  memset(&serv_address, 0, sizeof(serv_address));
+  serv_address.sin_family = AF_INET;
+  serv_address.sin_addr.s_addr = inet_addr(argv[1]);
+  serv_address.sin_port = htons(atoi(argv[2]));
+
+  int addrlen = sizeof(serv_address);
+  char buffer[1024] = {0};
+
+  // 创建套接字
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  ERROR_CHECK(sock, -1, "socket");
+
+  // 连接到服务器
+  int ret = connect(sock, (struct sockaddr *)&serv_address, addrlen);
+  ERROR_CHECK(ret, -1, "connet");
+
+  // 建立通信
+  recv(sock, buffer, sizeof(buffer), 0);
+  printf("%s\n", buffer);
+  fd_set set;
+
+  while (1) {
+    FD_ZERO(&set);
+    FD_SET(STDIN_FILENO, &set);
+    FD_SET(sock, &set);
+    select(sock + 1, &set, NULL, NULL, NULL);
+    if (FD_ISSET(STDIN_FILENO, &set)) {
+      memset(buffer, 0, sizeof(buffer));
+      read(STDIN_FILENO, buffer, sizeof(buffer));
+      if (!strcmp(buffer, "exit\n")) {
+        break;
+      }
+      send(sock, buffer, strlen(buffer) - 1, 0);
+    }
+    if (FD_ISSET(sock, &set)) {
+      memset(buffer, 0, sizeof(buffer));
+      ret = recv(sock, buffer, sizeof(buffer), 0);
+      if (0 == ret) {
+        break;
+      }
+      printf("Message from server:\n%s\n", buffer);
+    }
+  }
+  // 关闭
+  printf("------ Connection broke ------\n");
+  close(sock);
+
+  return 0;
+}
---
+++ linux_learning/day27/setsockopt/tcp_chat_server.c
@@ -0,0 +1,69 @@
+#include <func.h>
+
+int main(int argc, char const *argv[]) {
+  ARGS_CHECK(argc, 3);
+  int server_fd, new_socket;
+  struct sockaddr_in address;
+  memset(&address, 0, sizeof(address));
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = inet_addr(argv[1]);
+  address.sin_port = htons(atoi(argv[2]));
+  int addrlen = sizeof(address);
+  char buffer[1024] = {0};
+  char *hello = "------ Connected with Server ------";
+
+  // 创建套接字
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  ERROR_CHECK(server_fd, -1, "socket");
+
+  // 修改套接字属性
+  int opt =1;
+  int ret = setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
+  ERROR_CHECK(ret, -1 ,"setsockopt");
+
+  // 绑定套接字
+  ret = bind(server_fd, (struct sockaddr *)&address, addrlen);
+  ERROR_CHECK(ret, -1, "bind");
+
+  // 监听连接
+  ret = listen(server_fd, 3);
+  ERROR_CHECK(ret, -1, "listen");
+
+  // 接受连接
+  new_socket =
+      accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+  ERROR_CHECK(new_socket, -1, "accept");
+
+  send(new_socket, hello, strlen(hello), 0);
+  printf("------ Connected with Client ------\n");
+  fd_set set;
+
+  while (1) {
+    FD_ZERO(&set);
+    FD_SET(STDIN_FILENO, &set);
+    FD_SET(new_socket, &set);
+    select(new_socket + 1, &set, NULL, NULL, NULL);
+    if (FD_ISSET(STDIN_FILENO, &set)) {
+      memset(buffer, 0, sizeof(buffer));
+      read(STDIN_FILENO, buffer, sizeof(buffer));
+      if (!strcmp(buffer, "exit\n")) {
+        break;
+      }
+      send(new_socket, buffer, strlen(buffer) - 1, 0);
+    }
+    if (FD_ISSET(new_socket, &set)) {
+      memset(buffer, 0, sizeof(buffer));
+      ret = recv(new_socket, buffer, sizeof(buffer), 0);
+      if (0 == ret) {
+        break;
+      }
+      printf("Message from client:\n%s\n", buffer);
+    }
+  }
+  // 关闭
+  printf("------ Connection broke ------\n");
+  close(new_socket);
+  close(server_fd);
+
+  return 0;
+}
